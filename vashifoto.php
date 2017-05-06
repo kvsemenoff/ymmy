b1aa01925c3bf9c41ed66a1cf3b83c4fc5bec4d7
boka <web_masters_07@mail.ru>
2017-05-06 12:55:43 +0300

    struktura2

---
+++ vashifoto.php
@@ -0,0 +1,20 @@
+<?php require_once('includes/header.php'); ?>
+<div class="n-wrapper">
+	<div class="n-header">
+		<?php require_once('includes/header-top.php'); ?>
+	</div>
+	<div class="n-content">
+		<div class="n-content-inner vertical-align">
+
+		</div>
+	</div>
+	<div class="n-footer">
+		<?php require_once('includes/footer.php'); ?>	
+	</div>
+</div>
+</body>
+</html>
+
+
+
+
