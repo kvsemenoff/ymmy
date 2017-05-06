758a85520233b5b47f01dfc1597d75acc3c82a18
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-05-06 19:23:54 +0600

    90%

--- js/fonts.js
+++ js/fonts.js
@@ -1,6 +1,6 @@
 
  		$(document).ready(function() {
  			$("head").append('<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">');
- 			// $("head").append("<link href='//netdna.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.css' rel='stylesheet'>");
+ 			$("head").append("<link href='//netdna.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.css' rel='stylesheet'>");
  		}); 
  		
