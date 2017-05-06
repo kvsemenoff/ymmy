b66a9a703e41cc9c0d48fa1af2ec6ce388ca3856
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-05-06 16:37:30 +0600

    video

--- video.php
+++ video.php
@@ -1,11 +1,72 @@
 <?php require_once('includes/header.php'); ?>
-<div class="n-wrapper">
+<div class="n-wrapper video-page">
 	<div class="n-header">
 		<?php require_once('includes/header-top.php'); ?>
 	</div>
 	<div class="n-content">
 		<div class="n-content-inner vertical-align">
-	
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<span class="video_descr">Рубрика</span>
+						<h2 class="h2">Видео дня</h2>
+						<div class="video video_big">
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/XvVij3q0sk0" frameborder="0" allowfullscreen></iframe>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="video video_sm">
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/ej5BTV1B-PI?rel=0" frameborder="0" allowfullscreen></iframe>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="video video_sm">
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/6ckuzm79qyk?rel=0" frameborder="0" allowfullscreen></iframe>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="video video_sm">
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/a_P-CeolWAY?rel=0" frameborder="0" allowfullscreen></iframe>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="video video_sm">
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/s9e9p_nTNMc?rel=0" frameborder="0" allowfullscreen></iframe>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="video video_sm">
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/ret0VeKHlOQ?rel=0" frameborder="0" allowfullscreen></iframe>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="video video_sm">
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/ydDMgEcAYjc?rel=0" frameborder="0" allowfullscreen></iframe>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="video video_sm">
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/3ibaMTUGA58?rel=0" frameborder="0" allowfullscreen></iframe>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="video video_sm">
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/GpJm2nIbUhs?rel=0" frameborder="0" allowfullscreen></iframe>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="video video_sm">
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/N9Ml-1yETcw?rel=0" frameborder="0" allowfullscreen></iframe>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 	<div class="n-footer">
