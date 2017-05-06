a6ce741cba115f98aedd62910136a0308948eaa2
boka <web_masters_07@mail.ru>
2017-05-06 14:34:36 +0300

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,23 @@
-
- 		<!-- JS_BLOCK -->
- 		<script src="libs/jquery/jquery-1.11.1.min.js"></script>
- 		<script src="libs/owl.carousel/owl.carousel.js"></script>
- 		<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
- 		<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
- 		<script src="js/responsiveTabs.js"></script>
- 		<script src="js/jquery.maskedinput.min.js"></script>
- 		<script src="js/fonts.js"></script>
- 		<script src="js/common.js"></script>
- 		
+<div class="footer footer_padding footer_view">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="footer-box">
+					<img src="img/footer-logo.png" alt="img" class="footer__logo">
+					<p class="footer__address">
+						<span class="footer__span1">YMMY</span>SHOP<span class="footer__span2">.RU</span>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- JS_BLOCK -->
+<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+<script src="libs/owl.carousel/owl.carousel.js"></script>
+<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
+<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+<script src="js/responsiveTabs.js"></script>
+<script src="js/jquery.maskedinput.min.js"></script>
+<script src="js/fonts.js"></script>
+<script src="js/common.js"></script>
