43497e0a125edf5c883a2e73171789db242fbd33
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-05-06 18:53:59 +0600

    some

--- includes/header-top.php
+++ includes/header-top.php
@@ -13,7 +13,14 @@
 					</a>
 				</div>
 				<div class="menu-wrap">
-					
+					<div class="main-menu-burger">
+						<a href="#"><img src="../../img/burger.png" alt="img"><span>Меню</span></a>
+					</div>
+					<ul class="main-menu main-menu_display main-menu_padding">
+						<li class="main-menu__item"><a href="#" class="main-menu__link">Зоомагазин</a></li>
+						<li class="main-menu__item"><a href="#" class="main-menu__link">Ваши питомцы</a></li>
+						<li class="main-menu__item"><a href="#" class="main-menu__link">Видео дня</a></li>
+					</ul>
 				</div>
 				<h1 class="h1">Клуб владельцев <br> домашних животных и зоомагазин</h1>
 				<div class="button-wrap">
--- index.php
+++ index.php
@@ -5,16 +5,8 @@
 	</div>
 	<div class="n-content">
 		<div class="n-content-inner vertical-align">
-			<div class="main">
-				<div class="container">
-					<div class="h2">Каталог товаров</div>
-					<div class="button-wrap">
-						<a href="#" class="button button_basket"><i class="fa fa-shopping-cart" aria-hidden="true"></i>Корзина (0)</a>
-					</div>
-				</div>
-				<?php require_once('includes/tovar.php'); ?>
-				<?php require_once('includes/catalog.php'); ?>
-			</div>
+			<?php require_once('includes/tovar.php'); ?>
+			<?php require_once('includes/catalog.php'); ?>
 		</div>
 	</div>
 	<div class="n-footer">
