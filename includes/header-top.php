6f6730ed539b88bad2e384a06b70e9843004999c
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-05-06 18:08:36 +0600

    85% of header-top

---
+++ includes/header-top.php
@@ -0,0 +1,27 @@
+<?php require_once('includes/header.php'); ?>
+
+<div class="header-top">
+	<div class="header-color">
+		<div class="container">
+			<div class="row">
+				<div class="logo">
+					<a href="#">
+						<img src="img/footer-logo.png" alt="img" class="footer__logo">
+						<p class="footer__address">
+							<span class="footer__span1">YMMY</span>SHOP<span class="footer__span2">.RU</span>
+						</p>
+					</a>
+				</div>
+				<div class="menu-wrap">
+					
+				</div>
+				<h1 class="h1">Клуб владельцев <br> домашних животных и зоомагазин</h1>
+				<div class="button-wrap">
+					<a href="#" class="button button_main">Каталог товаров</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php require_once('includes/footer.php'); ?>
